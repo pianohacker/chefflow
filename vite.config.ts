5e7f05fdd7fbdb1d255c59ebf1f24c046a187ebe
Jesse Weaver <pianohacker@gmail.com>
2025-02-15 19:16:10 -0700

    Use native top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   optimizeDeps: {
     exclude: ["brotli-wasm", "brotli-wasm/pkg.bundler/brotli_wasm_bg.wasm"],
   },
-  plugins: [react(), topLevelAwait()],
+  plugins: [react()],
 });
